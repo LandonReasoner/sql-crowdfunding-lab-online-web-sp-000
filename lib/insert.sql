3e925f63a2619da2984ca1c8fb91078295076bf3
Landon Reasoner <reasoner.landon90@gmail.com>
2021-03-13 18:25:29 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,7 +5,7 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUE
 ('Roo Fest', 'Music', 500000, '6/15/20', '6/18/20'),
 ('Elephant Fest', 'Music', 25000, '7/15/20', '7/15/20'),
 ('Franks on a Stick', 'Food', 1000000, '6/1/16', '6/1/17'),
-('Goose in a Box', 'Animals', 5000, '4/1/16', '11/1/16'),
+('Goose Fest', 'Music', 50000, '8/1/20', '8/5/20'),
 ('Hall of Fame', 'Music', 100000, '6/1/16', '12/1/16'),
 ('I Wrote a book on SQL', 'Book', 5000, '9/1/16', '10/1/16'),
 ("Just kidding, I didn't", 'Book', 6000, '10/1/16', '12/1/16');
