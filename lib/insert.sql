87632f2b168ef6ef5cd13ff23b015a22c93ad579
Landon Reasoner <reasoner.landon90@gmail.com>
2021-03-13 18:24:40 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
 ('Forest Gym', 'Recreation', 100000, '6/1/12', '12/1/16'),
 ('Food Drive', 'Food', 200000, '4/1/20', '4/23/20'),
-('Run For a Cure', 'Fund Raiser', 500000, '6/1/20', '12/1/20'),
-('Duck Breeding', 'Book', 50000, '6/15/16', '8/15/16'),
-('Elephant Ears', 'Music', 25000, '7/15/16', '12/15/16'),
+('Run For a Cure', 'Fund Raiser', 500000, '6/1/20', '6/3/20'),
+('Roo Fest', 'Music', 500000, '6/15/20', '6/18/20'),
+('Elephant Fest', 'Music', 25000, '7/15/20', '7/15/20'),
 ('Franks on a Stick', 'Food', 1000000, '6/1/16', '6/1/17'),
 ('Goose in a Box', 'Animals', 5000, '4/1/16', '11/1/16'),
 ('Hall of Fame', 'Music', 100000, '6/1/16', '12/1/16'),
